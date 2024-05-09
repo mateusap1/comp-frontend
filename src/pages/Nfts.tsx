4d93fcb0424084a23061aa843a91c802145ef20a
Mateus Santos <santos.mateus@protonmail.com>
2024-05-09 13:46:27 -0300

    made list nfts prettier

--- src/pages/Nfts.tsx
+++ src/pages/Nfts.tsx
@@ -4,6 +4,18 @@ import { Button } from "../components/Button";
 import { useWallet, NftRole, NftInfo } from "../contexts/WalletProvider";
 import { Navbar } from "../components/Navbar";
 
+const truncateString = (input: string, maxLength: number): string => {
+  if (input.length <= maxLength) {
+    return input; // Return the original string if it's already shorter than maxLength
+  }
+
+  const startLength = Math.ceil((maxLength - 3) / 2); // Length of the start portion
+  const endLength = Math.floor((maxLength - 3) / 2); // Length of the end portion
+
+  const truncatedString = input.slice(0, startLength) + '...' + input.slice(-endLength);
+  return truncatedString;
+}
+
 const NFTs = () => {
   const [nfts, setNfts] = useState<NftInfo[] | null>(null);
   const [userNfts, setUserNfts] = useState<string[] | null>(null)
@@ -12,8 +24,6 @@ const NFTs = () => {
     currentWallet,
     loadScriptInfo,
     scriptInfo,
-    getPriceByRole,
-    buyRoleNft,
     getUserNfts,
     getAllNfts,
     listNft,
@@ -53,21 +63,24 @@ const NFTs = () => {
       <div className="w-full justify-center flex p-8 flex flex-col">
         <div>
           <h1>All Minted NFTs</h1>
-          <div className="flex flex-row gap-4">
+          <div className="flex flex-wrap gap-4">
             {(nfts !== null) && (userNfts !== null) && nfts.map(({ assetName, role, isListed, votes }) => (
-              <div className="p-4 flex flex-col bg-slate-100">
+              <div className="p-4 flex flex-col bg-slate-100 text-xl">
+                <div className="flex w-full justify-center">
+                  <img className="w-32 h-32 mb-8" src="https://storage.googleapis.com/jpeg-optim-files/d911ee3a-80c2-45a1-b278-29b31a3abab6"/>
+                </div>
                 <div className="flex flex-row gap-2">
                   <span>AssetName:</span>
-                  <span>{assetName}</span>
+                  <span>{truncateString(assetName, 20)}</span>
                 </div>
                 <div className="flex flex-row gap-2">
                   <span>Role:</span>
                   <span>{role}</span>
                 </div>
                 <div className="flex flex-row gap-2">
-                  <span>{isListed ? "LISTED" : "NOT LISTED"}</span>
+                  <span>{isListed ? "Listed" : "Not listed"}</span>
                 </div>
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row gap-2 mb-8">
                   <span>votes:</span>
                   <span>{votes.length}</span>
                 </div>
